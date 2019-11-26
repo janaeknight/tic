63ef5ef2151b6371451fe5d3f04fa3621449d532
Knight <janae.jpeg@gmail.com>
2019-11-25 23:00:47 -0600

    gen update

--- java/javascript.js
+++ java/javascript.js
@@ -2,48 +2,366 @@ var turns = 0;
 var userTurns = 0;
 var aiTurns = 0;
 
+// 🤔🤔🤔
+
 function tact1() {
     var tic = document.getElementById("t1");
         if (tic.innerText.length<1) {
             tic.innerHTML = "X";
             turns += 1;
             userTurns += 1;
+            function aiMove() {
+                let t1 = document.getElementById("t1");
+                let t2 = document.getElementById("t2");
+                let t3 = document.getElementById("t3");
+                let m1 = document.getElementById("m1");
+                let m2 = document.getElementById("m2");
+                let m3 = document.getElementById("m3");
+                let b1 = document.getElementById("b1");
+                let b2 = document.getElementById("b2");
+                let b3 = document.getElementById("b3");
+                    let spots = [t1, t2, t3, m1, m2, m3, b1, b2, b3];
+                    let rand = Math.random();
+                    let spotsSpx = spots.length;
+                    let randIndex = Math.floor(rand*spotsSpx);
+                    let randomSpot = spots[randIndex];
+                    console.log(randomSpot);
+            
+                    if (randomSpot.innerText.length<1 && turns<=8) {
+                        randomSpot.innerHTML = "O";
+                        aiTurns += 1;
+                        return;
+                    } else {
+                        aiMove();
+                    }
+                return;
+            }
+            aiMove();
+        }
+    console.log("Your turns: " +userTurns,);
+    console.log("Computer turns: " +aiTurns,);
+    return;
+}
+function tact2() {
+    var tic = document.getElementById("t2");
+        if (tic.innerText.length<1) {
+            tic.innerHTML = "X";
+            turns += 1;
+            userTurns += 1;
+            function aiMove() {
+                let t1 = document.getElementById("t1");
+                let t2 = document.getElementById("t2");
+                let t3 = document.getElementById("t3");
+                let m1 = document.getElementById("m1");
+                let m2 = document.getElementById("m2");
+                let m3 = document.getElementById("m3");
+                let b1 = document.getElementById("b1");
+                let b2 = document.getElementById("b2");
+                let b3 = document.getElementById("b3");
+                    let spots = [t1, t2, t3, m1, m2, m3, b1, b2, b3];
+                    let rand = Math.random();
+                    let spotsSpx = spots.length;
+                    let randIndex = Math.floor(rand*spotsSpx);
+                    let randomSpot = spots[randIndex];
+                    console.log(randomSpot);
+            
+                    if (randomSpot.innerText.length<1 && turns<=8) {
+                        randomSpot.innerHTML = "O";
+                        aiTurns += 1;
+                        return;
+                    } else {
+                        aiMove();
+                    }
+                return;
+            }
+            aiMove();
+        }
+    console.log("Your turns: " +userTurns,);
+    console.log("Computer turns: " +aiTurns,);
+    return;
+}
+function tact3() {
+    var tic = document.getElementById("t3");
+        if (tic.innerText.length<1) {
+            tic.innerHTML = "X";
+            turns += 1;
+            userTurns += 1;
+            function aiMove() {
+                let t1 = document.getElementById("t1");
+                let t2 = document.getElementById("t2");
+                let t3 = document.getElementById("t3");
+                let m1 = document.getElementById("m1");
+                let m2 = document.getElementById("m2");
+                let m3 = document.getElementById("m3");
+                let b1 = document.getElementById("b1");
+                let b2 = document.getElementById("b2");
+                let b3 = document.getElementById("b3");
+                    let spots = [t1, t2, t3, m1, m2, m3, b1, b2, b3];
+                    let rand = Math.random();
+                    let spotsSpx = spots.length;
+                    let randIndex = Math.floor(rand*spotsSpx);
+                    let randomSpot = spots[randIndex];
+                    console.log(randomSpot);
+            
+                    if (randomSpot.innerText.length<1 && turns<=8) {
+                        randomSpot.innerHTML = "O";
+                        aiTurns += 1;
+                        return;
+                    } else {
+                        aiMove();
+                    }
+                return;
+            }
+            aiMove();
+        }
+    console.log("Your turns: " +userTurns,);
+    console.log("Computer turns: " +aiTurns,);
+    return;
+}
+function tacm1() {
+    var tic = document.getElementById("m1");
+        if (tic.innerText.length<1) {
+            tic.innerHTML = "X";
+            turns += 1;
+            userTurns += 1;
+            function aiMove() {
+                let t1 = document.getElementById("t1");
+                let t2 = document.getElementById("t2");
+                let t3 = document.getElementById("t3");
+                let m1 = document.getElementById("m1");
+                let m2 = document.getElementById("m2");
+                let m3 = document.getElementById("m3");
+                let b1 = document.getElementById("b1");
+                let b2 = document.getElementById("b2");
+                let b3 = document.getElementById("b3");
+                    let spots = [t1, t2, t3, m1, m2, m3, b1, b2, b3];
+                    let rand = Math.random();
+                    let spotsSpx = spots.length;
+                    let randIndex = Math.floor(rand*spotsSpx);
+                    let randomSpot = spots[randIndex];
+                    console.log(randomSpot);
+            
+                    if (randomSpot.innerText.length<1 && turns<=8) {
+                        randomSpot.innerHTML = "O";
+                        aiTurns += 1;
+                        return;
+                    } else {
+                        aiMove();
+                    }
+                return;
+            }
+            aiMove();
+        }
+    console.log("Your turns: " +userTurns,);
+    console.log("Computer turns: " +aiTurns,);
+    return;
+}
+function tacm2() {
+    var tic = document.getElementById("m2");
+        if (tic.innerText.length<1) {
+            tic.innerHTML = "X";
+            turns += 1;
+            userTurns += 1;
+            function aiMove() {
+                let t1 = document.getElementById("t1");
+                let t2 = document.getElementById("t2");
+                let t3 = document.getElementById("t3");
+                let m1 = document.getElementById("m1");
+                let m2 = document.getElementById("m2");
+                let m3 = document.getElementById("m3");
+                let b1 = document.getElementById("b1");
+                let b2 = document.getElementById("b2");
+                let b3 = document.getElementById("b3");
+                    let spots = [t1, t2, t3, m1, m2, m3, b1, b2, b3];
+                    let rand = Math.random();
+                    let spotsSpx = spots.length;
+                    let randIndex = Math.floor(rand*spotsSpx);
+                    let randomSpot = spots[randIndex];
+                    console.log(randomSpot);
+            
+                    if (randomSpot.innerText.length<1 && turns<=8) {
+                        randomSpot.innerHTML = "O";
+                        aiTurns += 1;
+                        return;
+                    } else {
+                        aiMove();
+                    }
+                return;
+            }
+            aiMove();
+        }
+    console.log("Your turns: " +userTurns,);
+    console.log("Computer turns: " +aiTurns,);
+    return;
+}
+function tacm3() {
+    var tic = document.getElementById("m3");
+        if (tic.innerText.length<1) {
+            tic.innerHTML = "X";
+            turns += 1;
+            userTurns += 1;
+            function aiMove() {
+                let t1 = document.getElementById("t1");
+                let t2 = document.getElementById("t2");
+                let t3 = document.getElementById("t3");
+                let m1 = document.getElementById("m1");
+                let m2 = document.getElementById("m2");
+                let m3 = document.getElementById("m3");
+                let b1 = document.getElementById("b1");
+                let b2 = document.getElementById("b2");
+                let b3 = document.getElementById("b3");
+                    let spots = [t1, t2, t3, m1, m2, m3, b1, b2, b3];
+                    let rand = Math.random();
+                    let spotsSpx = spots.length;
+                    let randIndex = Math.floor(rand*spotsSpx);
+                    let randomSpot = spots[randIndex];
+                    console.log(randomSpot);
+            
+                    if (randomSpot.innerText.length<1 && turns<=8) {
+                        randomSpot.innerHTML = "O";
+                        aiTurns += 1;
+                        return;
+                    } else {
+                        aiMove();
+                    }
+                return;
+            }
+            aiMove();
+        }
+    console.log("Your turns: " +userTurns,);
+    console.log("Computer turns: " +aiTurns,);
+    return;
+}
+function tacb1() {
+    var tic = document.getElementById("b1");
+        if (tic.innerText.length<1) {
+            tic.innerHTML = "X";
+            turns += 1;
+            userTurns += 1;
+            function aiMove() {
+                let t1 = document.getElementById("t1");
+                let t2 = document.getElementById("t2");
+                let t3 = document.getElementById("t3");
+                let m1 = document.getElementById("m1");
+                let m2 = document.getElementById("m2");
+                let m3 = document.getElementById("m3");
+                let b1 = document.getElementById("b1");
+                let b2 = document.getElementById("b2");
+                let b3 = document.getElementById("b3");
+                    let spots = [t1, t2, t3, m1, m2, m3, b1, b2, b3];
+                    let rand = Math.random();
+                    let spotsSpx = spots.length;
+                    let randIndex = Math.floor(rand*spotsSpx);
+                    let randomSpot = spots[randIndex];
+                    console.log(randomSpot);
+            
+                    if (randomSpot.innerText.length<1 && turns<=8) {
+                        randomSpot.innerHTML = "O";
+                        aiTurns += 1;
+                        return;
+                    } else {
+                        aiMove();
+                    }
+                return;
+            }
+            aiMove();
+        }
+    console.log("Your turns: " +userTurns,);
+    console.log("Computer turns: " +aiTurns,);
+    return;
+}
+function tacb2() {
+    var tic = document.getElementById("b2");
+        if (tic.innerText.length<1) {
+            tic.innerHTML = "X";
+            turns += 1;
+            userTurns += 1;
+            function aiMove() {
+                let t1 = document.getElementById("t1");
+                let t2 = document.getElementById("t2");
+                let t3 = document.getElementById("t3");
+                let m1 = document.getElementById("m1");
+                let m2 = document.getElementById("m2");
+                let m3 = document.getElementById("m3");
+                let b1 = document.getElementById("b1");
+                let b2 = document.getElementById("b2");
+                let b3 = document.getElementById("b3");
+                    let spots = [t1, t2, t3, m1, m2, m3, b1, b2, b3];
+                    let rand = Math.random();
+                    let spotsSpx = spots.length;
+                    let randIndex = Math.floor(rand*spotsSpx);
+                    let randomSpot = spots[randIndex];
+                    console.log(randomSpot);
+            
+                    if (randomSpot.innerText.length<1 && turns<=8) {
+                        randomSpot.innerHTML = "O";
+                        aiTurns += 1;
+                        return;
+                    } else {
+                        aiMove();
+                    }
+                return;
+            }
+            aiMove();
+        }
+    console.log("Your turns: " +userTurns,);
+    console.log("Computer turns: " +aiTurns,);
+    return;
+}
+function tacb3() {
+    var tic = document.getElementById("b3");
+        if (tic.innerText.length<1) {
+            tic.innerHTML = "X";
+            turns += 1;
+            userTurns += 1;
+            function aiMove() {
+                let t1 = document.getElementById("t1");
+                let t2 = document.getElementById("t2");
+                let t3 = document.getElementById("t3");
+                let m1 = document.getElementById("m1");
+                let m2 = document.getElementById("m2");
+                let m3 = document.getElementById("m3");
+                let b1 = document.getElementById("b1");
+                let b2 = document.getElementById("b2");
+                let b3 = document.getElementById("b3");
+                    let spots = [t1, t2, t3, m1, m2, m3, b1, b2, b3];
+                    let rand = Math.random();
+                    let spotsSpx = spots.length;
+                    let randIndex = Math.floor(rand*spotsSpx);
+                    let randomSpot = spots[randIndex];
+                    console.log(randomSpot);
+            
+                    if (randomSpot.innerText.length<1 && turns<=8) {
+                        randomSpot.innerHTML = "O";
+                        aiTurns += 1;
+                        return;
+                    } else {
+                        aiMove();
+                    }
+                return;
+            }
+            aiMove();
         }
     console.log("Your turns: " +userTurns,);
     console.log("Computer turns: " +aiTurns,);
     return;
 }
 
-function aiMove() {
-    let t1 = document.getElementById("t1");
-    let t2 = document.getElementById("t2");
-    let t3 = document.getElementById("t3");
-    let m1 = document.getElementById("m1");
-    let m2 = document.getElementById("m2");
-    let m3 = document.getElementById("m3");
-    let b1 = document.getElementById("b1");
-    let b2 = document.getElementById("b2");
-    let b3 = document.getElementById("b3");
-        let spots = [t1, t2, t3, m1, m2, m3, b1, b2, b3];
-        let rand = Math.random();
-        let spotsSpx = spots.length;
-        let randIndex = Math.floor(rand*spotsSpx);
-        let randomSpot = spots[randIndex];
-        console.log(randomSpot);
-
-        if (randomSpot.innerText.length<1) {
-            randomSpot.innerHTML = "O";
-            aiTurns += 1;
-            return;
-        } else {
-            aiMove();
-        }
-    return;
-}
-
+// All Possible Matches ((Manual))
 function checkWin() {
     if (turns>=3) {
         console.log("Checking possible solutions...");
+        if ( ((t1="X") && (t2="X") && (t3="X")) || 
+            ((m1="X") && (m2="X") && (m3="X")) || 
+            ((b1="X") && (b2="X") && (b3="X")) || 
+            ((t1="X") && (m1="X") && (b1="X")) || 
+            ((t2="X") && (m2="X") && (b2="X")) || 
+            ((t3="X") && (m3="X") && (b3="X")) || 
+            ((t1="X") && (m2="X") && (b3="X")) || 
+            ((t4="X") && (m2="X") && (b1="X"))
+        ) {
+            console.log("X wins!")
+        }
     } else if (turns===9) {
         console.log("Draw!");
     };
