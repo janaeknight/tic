484f7ac4b422340316b8bf7341fdfb86b2e2141e
Knight <janae.jpeg@gmail.com>
2019-11-25 20:59:35 -0600

    gen update

--- java/javascript.js
+++ java/javascript.js
@@ -1,12 +1,67 @@
 var turns = 0; 
+var userTurns = 0;
+var aiTurns = 0;
 
-function tac1() {
-    var tic = document.getElementById("b1");
-    tic.innerHTML = "x";
-    turns += 1;
-    console.log(turns);
+function tact1() {
+    var tic = document.getElementById("t1");
+        if (tic.length>1) {
+            tic.innerHTML = "x";
+            turns += 1;
+            userTurns += 1;
+        };
+    console.log("Your turns: " +userTurns,);
+    console.log("Computer turns: " +aiTurns,);
     return;
 }
+
+
+
+function checkWin() {
+    if (turns>=3) {
+        console.log("Checking possible solutions...");
+    } else if (turns===9) {
+        console.log("Draw!");
+    };
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function clearBoard() {
     // var board = document.getElementsByClassName("spx");
