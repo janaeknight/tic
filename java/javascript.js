a9e40ab55ead01d166f4ae027256e7bea60fb9da
Knight <janae.jpeg@gmail.com>
2019-11-25 20:20:12 -0600

    gen update

---
+++ java/javascript.js
@@ -0,0 +1,14 @@
+var turns = 0; 
+
+function tac1() {
+    var tic = document.getElementById("b1");
+    tic.innerHTML = "x";
+    turns += 1;
+    console.log(turns);
+    return;
+}
+
+function clearBoard() {
+    // var board = document.getElementsByClassName("spx");
+    // board.innerHTML  = "here";
+}
